ead097453056d0f082ccd054d926f97dae9620a9
unknown <Anwarqarout@gmail.com>
2021-08-01 11:04:42 +0300

    modified: added all functionalities except delete

--- TestKafka.py
+++ TestKafka.py
@@ -13,54 +13,64 @@ class TestKafka:
         self.admin = kafka.admin.KafkaAdminClient(bootstrap_servers="localhost:9092"
                                                   , client_id="www"
                                                   )
-        self.consumer = kafka.consumer.KafkaConsumer(bootstrap_servers="localhost:9092")
-        self.producer = kafka.producer.KafkaProducer(bootstrap_servers="localhost:9092")
         print("done")
 
     def create_topic(self, topic):
         topics = []
         topics.append(kafka.admin.NewTopic(name=topic, num_partitions=1, replication_factor=1))
-        self.admin.create_topics(new_topics=topic)
+        try:
+            self.admin.create_topics(new_topics=topics)
+        except:
+            print("An error occurred while creating a topic. A topic with that name may already exist.")
 
     def delete_topic(self, topic):
-        topiccs = []
-        topiccs.append(kafka.admin.NewTopic(name=topic, num_partitions=1, replication_factor=1))
-        self.admin.delete_topics(topics=topic)
+        topics = []
+        topics.append(topic)
+        self.admin.delete_topics(topics=topics, timeout_ms=2000)
 
-    def list_all_topics(self, host):
+    def list_all_topics(host):
         topics = host.admin.list_topics()
         return topics
 
-    def send_file_to_kafka_topic(self,topic,filename):
-        file = open(filename,"r")
-        str = file.read()
-        str=json_serializer(str)
-        print(str)
-        self.producer.send(topic, value=b'hiiiiiiiiiiiiii')
+    def send_file_to_kafka_topic(self, topic, filename):
+        producer = kafka.producer.KafkaProducer(bootstrap_servers="localhost:9092", value_serializer=lambda x:
+        json.dumps(x).encode('utf-8'))
 
-    def kafka_consumer(self,topic,filename):
+        """consumer = kafka.consumer.KafkaConsumer(topic,
+                                                bootstrap_servers=['localhost:9092'],
+                                                auto_offset_reset='earliest',
+                                                enable_auto_commit=True,
+                                                group_id='my-group',
+                                                value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+                                                )"""
+
         file = open(filename, "r")
         str = file.read()
-        str = json_serializer(str)
-        topiccs = []
-        topiccs.append(topic)
-        print(self.consumer.bootstrap_connected())
-        print(self.consumer.topics())
-        output = self.consumer.poll()
-        print(output)
+        producer.send(topic=topic, value=str)
+        # consumer.poll()
+
+    def kafka_consumer(self, topic, filename):
+        consumer = kafka.consumer.KafkaConsumer(topic, bootstrap_servers=['localhost:9092'],
+                                                auto_offset_reset='earliest',
+                                                enable_auto_commit=True, consumer_timeout_ms=5000,
+                                                value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+        file = open(filename, "w+")
+        output = ""
+        for message in consumer:
+            output += message.value
+
+        file.write(output)
 
 
 
-    # def delete_topic(self,topic):
-
-    # def kafka_consumer(self,host,topic,filename):
-
-
-k = TestKafka()
-# k.create_topic("hahha")
-#print(k.list_all_topics(k))
-#k.delete_topic(["TestTodasdpic"])
-#print(k.list_all_topics(k))
-#print(k.list_all_topics(k))
-k.send_file_to_kafka_topic("rofl","file.txt")
-#k.kafka_consumer("TestTopic","file.txt")
+# k = TestKafka()
+# k.create_topic("tempTopic")
+# print(k.list_all_topics())
+# k.delete_topic(["TestTodasdpic"])
+# print(k.list_all_topics(k))
+# print(k.list_all_topics(k))
+# k.delete_topic('rofl')
+# print("all done!!!")
+# print(k.list_all_topics())
+# k.send_file_to_kafka_topic("top1","file.txt")
+# k.kafka_consumer("top1","file20.txt")
